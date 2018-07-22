4a40c7500ff0520db14fe2b819e97c51305db9b2
Zhiliang Gong <zhiliang.gong@gmail.com>
2018-07-22 10:48:26 -0500

    add pair solution to twoSum

--- C++/two-sum.cpp
+++ C++/two-sum.cpp
@@ -13,4 +13,15 @@ public:
         }
         return {};
     }
+    pair<int, int> twoSum2(const vector<int> & nums, int target) {
+        unordered_map<int, int> lookup;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (lookup.count(target - nums[i])) {
+                return { lookup[target - nums[i]], i };
+            } else {
+                lookup[nums[i]] = i;
+            }
+        }
+        return {};
+    }
 };
