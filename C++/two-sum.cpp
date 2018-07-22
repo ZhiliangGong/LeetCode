000405ec5993dc9b77dec506a10c5b676164f773
Zhiliang Gong <zhiliang.gong@gmail.com>
2018-07-22 10:48:45 -0500

    add pair solution to twoSUm

--- C++/two-sum.cpp
+++ C++/two-sum.cpp
@@ -3,7 +3,7 @@
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(const vector<int> & nums, int target) {
         unordered_map<int, int> lookup;
         for (int i = 0; i < nums.size(); ++i) {
             if (lookup.count(target - nums[i])) {
