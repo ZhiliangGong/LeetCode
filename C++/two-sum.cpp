09b1f16e6b6bb5dfa3717ca8ece721c69c4036e9
Zhiliang Gong <zhiliang.gong@gmail.com>
2018-07-22 15:51:06 -0500

    remove additional twoSum method

--- C++/two-sum.cpp
+++ C++/two-sum.cpp
@@ -3,24 +3,13 @@
 
 class Solution {
 public:
-    vector<int> twoSum(const vector<int> & nums, int target) {
+    vector<int> twoSum(vector<int> & nums, int target) {
         unordered_map<int, int> lookup;
         for (int i = 0; i < nums.size(); ++i) {
             if (lookup.count(target - nums[i])) {
                 return {lookup[target - nums[i]], i};
             }
             lookup[nums[i]] = i;
-        }
-        return {};
-    }
-    pair<int, int> twoSum2(const vector<int> & nums, int target) {
-        unordered_map<int, int> lookup;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (lookup.count(target - nums[i])) {
-                return { lookup[target - nums[i]], i };
-            } else {
-                lookup[nums[i]] = i;
-            }
         }
         return {};
     }
